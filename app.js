62e3ecdd7e60e64adb6f8e080467e0093d64969f
Gulshan SharmA <gulshansharma011@gmail.com>
2023-10-18 19:16:23 +0530

    Aded all files

---
+++ app.js
@@ -0,0 +1,32 @@
+const buttons=document.querySelectorAll('button');
+const screenDispaly=document.querySelector('.screen');
+
+let result=[];
+let accumulativeCalculation
+function calculate(button){
+   try{
+    if(button == 'CLEAR'){
+        result=[];
+        screenDispaly.textContent = 0;
+    } else if(button === '='){
+        screenDispaly.textContent=eval(accumulativeCalculation);
+        result=[eval(accumulativeCalculation)]
+    } else {
+        result.push(button)
+    accumulativeCalculation = result.join('');
+    screenDispaly.textContent=accumulativeCalculation;
+    console.log(result);
+    }
+   } catch(error){
+    // screenDispaly.innerHTML = "Andha hai Kya!! \n Yedde";
+    // document.getElementById('image').style.display='block';
+    // <img id="image" src="error.png" style="display:none;"/>
+    screenDispaly.textContent = '';
+    var img = document.createElement("img");
+    img.setAttribute('id','image')
+    img.setAttribute('src', 'error.png');
+    screenDispaly.appendChild(img);
+   }
+}
+
+buttons.forEach(button => button.addEventListener('click',() => calculate(button.textContent)))
